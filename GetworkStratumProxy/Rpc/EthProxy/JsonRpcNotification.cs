c77eb0f36dde0484d12043bf398668c61af32c89
SergeantSerk <14278530+SergeantSerk@users.noreply.github.com>
2021-12-14 16:47:45 +0000

    Minor code cleanup

--- GetworkStratumProxy/Rpc/EthProxy/JsonRpcNotification.cs
+++ GetworkStratumProxy/Rpc/EthProxy/JsonRpcNotification.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GetworkStratumProxy.Rpc.EthProxy
+﻿namespace GetworkStratumProxy.Rpc.EthProxy
 {
     public class JsonRpcNotification : JsonRpcResponse
     {
