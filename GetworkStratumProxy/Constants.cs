bd3301b11b463d20834ec094b2060a46897118c4
SergeantSerk <14278530+SergeantSerk@users.noreply.github.com>
2022-09-15 01:12:00 +0100

    Added more crypto mathematical functions

--- GetworkStratumProxy/Constants.cs
+++ GetworkStratumProxy/Constants.cs
@@ -8,18 +8,29 @@ namespace GetworkStratumProxy
         public const int WorkHeaderCharactersPrefixCount = 10;
 
         private static readonly BigInteger MaxTarget = BigInteger.Pow(16, 64) - 1;
-        private static readonly BigInteger DifficultyDenom = BigInteger.Pow(2, 32);
+        private const long BaseDifficultyOfOne = 4294967296;
 
         public static decimal GetDifficultyFromTarget(HexBigInteger currentTarget)
         {
             var calculatedDifficulty = MaxTarget / currentTarget.Value;
-            return (decimal)calculatedDifficulty / (decimal)DifficultyDenom;
+            return (decimal)calculatedDifficulty / BaseDifficultyOfOne;
         }
 
         public static HexBigInteger GetTargetFromDifficulty(decimal difficulty)
         {
-            var calculatedDifficulty = difficulty * (decimal)DifficultyDenom;
+            var calculatedDifficulty = difficulty * BaseDifficultyOfOne;
             return new HexBigInteger(MaxTarget / (BigInteger)calculatedDifficulty);
+        }
+
+        public static decimal GetDifficultySize(HexBigInteger currentTarget)
+        {
+            var targetDiff = GetDifficultyFromTarget(currentTarget);
+            return BaseDifficultyOfOne * targetDiff;
+        }
+
+        public static decimal GetDifficultySize(decimal difficulty)
+        {
+            return BaseDifficultyOfOne * difficulty;
         }
     }
 }
